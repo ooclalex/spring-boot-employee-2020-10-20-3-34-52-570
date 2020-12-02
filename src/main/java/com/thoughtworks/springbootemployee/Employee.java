42f350f1f48403b92e24e05267a86392f1db70b6
Alex Liu <liual@oocl.com>
2020-12-02 15:05:50 +0800

    [SpringBootEmployee][Alex Liu] feat: added get employees

---
+++ src/main/java/com/thoughtworks/springbootemployee/Employee.java
@@ -0,0 +1,9 @@
+package com.thoughtworks.springbootemployee;
+
+public class Employee {
+    private int id;
+    private String name;
+    private int age;
+    private int salary;
+    private String gender;
+}
