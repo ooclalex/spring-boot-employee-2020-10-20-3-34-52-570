eb3d518bf95b08affad80aac3c8bfbb0c5b93828
Alex Liu <liual@oocl.com>
2020-12-03 22:52:07 +0800

    [SpringBootEmployee][Alex Liu] refactor: removed unused imports

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -1,16 +1,12 @@
 package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.Employee;
-import com.thoughtworks.springbootemployee.exception.NotFoundException;
 import com.thoughtworks.springbootemployee.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/employees")
@@ -40,6 +36,7 @@ public class EmployeeController {
     public Employee getSpecificEmployee(@PathVariable String employeeId) {
         return employeeService.getSpecificEmployee(employeeId);
     }
+
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
